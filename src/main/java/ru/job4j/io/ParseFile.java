43fd61215617c0d1609d7f28c3b5146e494ad431
ADIKIAA <zhekov.nick@yandex.ru>
2023-08-12 11:40:46 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102 #436173]

--- src/main/java/ru/job4j/io/ParseFile.java
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -10,19 +10,27 @@ public final class ParseFile {
         this.file = file;
     }
 
-    public String getContentWithPredicate(Predicate<Integer>  predicate) {
-        String output = "";
+    public String getContent() {
+        return getContentWithPredicate((s) -> true);
+    }
+
+    public String getContentWithoutUnicode() {
+        return getContentWithPredicate((s) -> s < 0x80);
+    }
+
+    private String getContentWithPredicate(Predicate<Integer>  predicate) {
+        StringBuilder output = new StringBuilder();
         try (InputStream i = new BufferedInputStream(new FileInputStream(file))) {
             int data;
-            while ((data = i.read()) > 0) {
+            while ((data = i.read()) != -1) {
                 if (predicate.test(data)) {
-                    output += (char) data;
+                    output.append((char) data);
                 }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return output;
+        return output.toString();
     }
 
 }
